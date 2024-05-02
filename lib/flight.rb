f34ce88806e6733e69d5a15564810030da87b40e
david30-maker <davidokpah396@gmail.com>
2024-05-02 15:10:45 +0100

    building flight management app

---
+++ lib/flight.rb
@@ -0,0 +1,4 @@
+# class Flight
+#   validates :flight_number, uniqueness: true
+#   flight_number, origin, destination
+# end
---
+++ lib/seats_generator.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# rubocop:disable Style/Documentation
+# class SeatsGenerator
+#   def create_seats(rows, seats_in_row, first_row_number)
+#     # TODO
+#     seats = []
+#     rows.times do |row|
+#       row_number = first_row_number + row 
+#       seats_in_row.times do |seat|
+#         seat_letter = (seat + 65).chr
+#         seats << "#{row_number}#{seat_letter}"
+#       end
+#     end
+#     seats 
+#   end
+# end
