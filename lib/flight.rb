864c0dd7108569ae23263c6d2395f72b6efa1caf
david30-maker <davidokpah396@gmail.com>
2024-05-02 15:14:39 +0100

    add validation to flight.rb file

--- lib/flight.rb
+++ lib/flight.rb
@@ -1,4 +1,4 @@
-# class Flight
-#   validates :flight_number, uniqueness: true
-#   flight_number, origin, destination
-# end
+class Flight
+  validates :flight_number, uniqueness: true
+  flight_number, origin, destination
+end
