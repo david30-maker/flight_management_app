7a16e95ee5433defff381792288e71c43a245bb0
david30-maker <davidokpah396@gmail.com>
2024-05-02 15:16:07 +0100

    add add_new_flight method to flightmanager class

--- lib/flight_manager.rb
+++ lib/flight_manager.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
 # rubocop:disable Style/Documentation
-# class FlightManager
-#   attr_accessor :flights
+class FlightManager
+  attr_accessor :flights
 
-#   def flights
-#     @_flights ||= []
-#   end
+  def flights
+    @_flights ||= []
+  end
 
-#   def add_new_flight(flight_number, origin, destination)
-#     if flights.select {|flight| flight[:flight_number] == flight_number}.size > 0
-#       return false
-#     else
-#       flight = flights << {flight_number: flight_number, origin: origin, destination: destination}
-#       return true
-#     end
-#   end
+  def add_new_flight(flight_number, origin, destination)
+    if flights.select {|flight| flight[:flight_number] == flight_number}.size > 0
+      return false
+    else
+      flight = flights << {flight_number: flight_number, origin: origin, destination: destination}
+      return true
+    end
+  end
 
-#   def find_flights_between(origin, destination, direction_sensitive)
-#     if direction_sensitive
-#       flights.select { |flight| flight[:origin] == origin && flight[:destination] == destination }
-#     else
-#       flights.select { |flight| (flight[:origin] == origin && flight[:destination] == destination) ||
-#         (flight[:origin] == destination && flight[:destination] == origin) }
-#     end
-#   end
-# end
+  # def find_flights_between(origin, destination, direction_sensitive)
+  #   if direction_sensitive
+  #     flights.select { |flight| flight[:origin] == origin && flight[:destination] == destination }
+  #   else
+  #     flights.select { |flight| (flight[:origin] == origin && flight[:destination] == destination) ||
+  #       (flight[:origin] == destination && flight[:destination] == origin) }
+  #   end
+  # end
+end
 # rubocop:enable Style/Documentation
